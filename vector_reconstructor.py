126031064f5d90b147525d8db3f0743898733ef3
Punished Benzene <pcvtermin@gmail.com>
2026-06-03 17:57:10 +0300

    Update vector_reconstructor.py

--- vector_reconstructor.py
+++ vector_reconstructor.py
@@ -2,12 +2,15 @@ import sys
 import os
 import pandas as pd
 import numpy as np
+import re
 
 from numpy import ndarray
 from pandas import DataFrame, ExcelFile, ExcelWriter
 from tkinter import filedialog
 from typing import IO, Optional, Any
 
+DIM_FIND_RE = r"(\d+)\s*[xX ]\s*(\d+)"
+AXES_FIND_RE = r"^(\d+(?:\.\d+)?)[ x](\d+(?:\.\d+)?)$"
 #Проверка на то, что скрипт был запущен как drad & drop
 DEV: bool = False
 
@@ -55,9 +58,9 @@ def do_exit_msg(msg: str):
         sys.exit()
     
 
-def work_with_excel_data(path: str, dimensions: tuple[int, int], axes: tuple[list[float], list[float]]) -> Optional[dict[str, DataFrame]]:
+def work_with_excel_data(path: str, dimensions: tuple[int, int], axis_X: Optional[tuple[float, float]], axis_Y: Optional[tuple[float,float]]) -> Optional[tuple[dict[str, DataFrame], list[str], list[str]]]:
     choosed_data: Optional[DataFrame] = get_list_file(path)
-    if (choosed_data is None):
+    if (choosed_data is None or axis_X is None or axis_Y is None):
         return None
     
     data_len: int = dimensions[0] * dimensions[1]
@@ -97,11 +100,16 @@ def work_with_excel_data(path: str, dimensions: tuple[int, int], axes: tuple[lis
     iterations: int = int(data_len / step)
     result_dataframes: dict[str, DataFrame] = {}
     
+    
+
     for label in list(choosed_data.columns):
         result_dataframes[label] = DataFrame()
         for i in range(1, iterations+1):
-            result_dataframes[label][i+1] = choosed_data[label].values[step*(i-1):step*(i)]
-    return result_dataframes
+            result_dataframes[label][i] = choosed_data[label].values[step*(i-1):step*(i)]
+
+    v = 1 if result == 0 else 0
+
+    return result_dataframes, np.linspace(axis_X[0], axis_X[1], dimensions[v]).astype(str).tolist(),np.linspace(axis_Y[0], axis_Y[1], dimensions[result]).astype(str).tolist()
     
 def save_at_script_path(data: dict[str, DataFrame]) -> str:
     return os.path.join(os.path.dirname(sys.argv[0]),"result.xlsx")
@@ -158,7 +166,7 @@ def get_list_file(path: str) -> Optional[DataFrame]:
 
     return needed_dataframe
 ## [0] - размерность [1] - оси
-def get_initial_dimensions() -> Optional[tuple[tuple[int, int], tuple[float, float]]]:
+def get_initial_dimensions() -> Optional[tuple[tuple[int, int], tuple[float, float], tuple[float, float]]]:
     #Справшиваем
 
 
@@ -196,7 +204,7 @@ def get_initial_dimensions() -> Optional[tuple[tuple[int, int], tuple[float, flo
             return 
 
         needed_dataframe: Optional[DataFrame] = get_list_file(path)
-
+        dimensions_Y: tuple[float, float] = (0,0)
         if (needed_dataframe is None):
             do_exit_msg("Aborting")
             return
@@ -207,50 +215,88 @@ def get_initial_dimensions() -> Optional[tuple[tuple[int, int], tuple[float, flo
 
         needed_dataframe.drop(cols_to_drop, axis=1, inplace=True)
 
-        input_res: str = input(f"{needed_dataframe.shape[0]} X {needed_dataframe.shape[1]} | TotalPoints: {needed_dataframe.shape[0] * needed_dataframe.shape[1]} is correct? Type 's' if no. Type any symbol if yes.\n")
+        dimensions_X: tuple[float, float] = (min(needed_dataframe.axes[1]), max(needed_dataframe.axes[1]))
+        dimensions_Y: tuple[float, float] = (min(needed_dataframe.axes[0]), max(needed_dataframe.axes[0]))
+
+        input_res: str = input(f"{needed_dataframe.shape[0]} X {needed_dataframe.shape[1]} | TotalPoints: {needed_dataframe.shape[0] * needed_dataframe.shape[1]} | Axes [X from {dimensions_X[0]} to {dimensions_X[1]}] [Y from {dimensions_Y[0]} to {dimensions_Y[1]}] \n is correct? Type 's' if no. Type any symbol if yes.\n")
 
         if (need_to_abort(input_res)):
             do_exit_msg('Aborting')
             return
         
-        return (needed_dataframe.shape[0], needed_dataframe.shape[1])
+
+
+        return (needed_dataframe.shape[0], needed_dataframe.shape[1]),dimensions_X,dimensions_Y
     else:
         input_row_int: int = -1
         input_collum_int: int = -1
+        axis_X: tuple[float, float]
+        axis_Y: tuple[float, float]
         while (True):
             while (True):
-                input_str: str = input("Write rows number or 's' to abort\n")
+                input_str: str = input("Write two dimensions (like: '46 30' or '46x30') or 's' to abort\n")
                 if (need_to_abort(input_str.lower())):
                     do_exit_msg('Aborting')
                     return
                 
-                if (not input_str.isdigit()):
-                    print(f"{input_str} is not a digt. Type 's' for abort")
-                    do_exit_msg("Aborting")
-                
-                input_row_int = int(input_str)
+                matched = re.findall(DIM_FIND_RE, input_str)
+
+                if (len(matched[0]) != 2):
+                    print(f"{input_str} is not correct! Dimensions should be like '47x94' or '48 53'")
+                    continue
+
+                input_row_int = int(matched[0][0])
+                input_collum_int = int(matched[0][1])
                 break
 
             while (True):
-                input_str: str = input("Write collums number or 's' to abort\n")
-                if (need_to_abort(input_str.lower())):
-                    print(f"{input_str} is not a digt. Type 's' for abort")
-                    do_exit_msg('Aborting')
-                    
-                
-                if (not input_str.isdigit()):
-                    print(f"{input_str} is not a digt. Type 's' for abort")
-                    break
+                input_str: str = input("Write axis X limits. For example: '100x200' or '200 400'. or 's' to abort\n")
 
-                input_collum_int = int(input_str)
+                if (need_to_abort(input_str.lower())):
+                    do_exit_msg('Aborting')
+                    return
+                
+                matched = re.findall(AXES_FIND_RE, input_str)
+
+                if (len(matched[0]) != 2):
+                    print(f"{input_str} is not correct! Dimensions should be like '47x94' or '48 53'")
+                    continue
+                
+                floated_tuple: list[float]= [float(matched[0][0]), float(matched[0][1])]
+
+                axis_X = (min(floated_tuple), max(floated_tuple))
+                if (axis_X[0] == axis_X[1]):
+                    print(f"Gotted equial axes. Aborting")
+                    continue
                 break
 
-            input_str: str = input(f"{input_row_int} X {input_collum_int} | TotalPoint {input_collum_int * input_row_int} is correct? Type 's' if no. Type anyting if yes.\n")
+            while (True):
+                input_str: str = input("Write axis Y limits. For example: '100x200' or '200 400'. or 's' to abort\n")
+
+                if (need_to_abort(input_str.lower())):
+                    do_exit_msg('Aborting')
+                    return
+                
+                matched = re.findall(AXES_FIND_RE, input_str)
+
+                if (len(matched[0]) != 2):
+                    print(f"{input_str} is not correct! Dimensions should be like '47x94' or '48 53'")
+                    continue
+                
+                floated_tuple: list[float]= [float(matched[0][0]), float(matched[0][1])]
+
+                axis_Y = (min(floated_tuple), max(floated_tuple))
+                if (axis_Y[0] == axis_Y[1]):
+                    print(f"Gotted equial axes. Aborting")
+                    continue
+                break
+
+            input_str: str = input(f"{input_row_int} X {input_collum_int} | TotalPoint {input_collum_int * input_row_int} | Axes [X from {axis_X[0]} to {axis_X[1]}] [Y from {axis_Y[0]} to {axis_Y[1]}] \n is correct? Type 's' if no. Type anyting if yes.\n")
 
             if (need_to_abort(input_str.lower())):
                 continue
 
-            return (input_row_int, input_collum_int)
+            return (input_row_int, input_collum_int),(axis_X[0],axis_X[1]),(axis_Y[0],axis_Y[1])
 
         
 
@@ -260,13 +306,19 @@ def get_initial_dimensions() -> Optional[tuple[tuple[int, int], tuple[float, flo
 def main() -> None:
     IS_DRAG_AND_DROPPED: bool = False
     print("Awaiting an spectra file")
+    
+    result_dimensions = get_initial_dimensions()
 
-    dimensions: Optional[tuple[int, int]] = get_initial_dimensions()
 
-    if (dimensions is None):
+    if (result_dimensions is None):
         do_exit_msg('Looks like something go wrongly...')
         return
     
+    dimensions: Optional[tuple[int, int]]
+    axis_X: Optional[tuple[float,float]]
+    axis_Y: Optional[tuple[float, float]]
+
+    dimensions, axis_X, axis_Y = result_dimensions
 
     filepath: Optional[str] =  check_is_drag_and_dropped()
     if (filepath is None): 
@@ -288,35 +340,40 @@ def main() -> None:
         do_exit_msg(f"The file format is wrong. Choose any another like {AVIABLE_FORMATS_ONLY_DOT}")
         return
     
-    product_dataframe: Optional[dict[str,DataFrame]]
-
+    product_dataframe: Optional[tuple[dict[str,DataFrame], list[str], list[str]]]
     
 
     if (check_format in EXCEL_FORMATS):
-        product_dataframe = work_with_excel_data(filepath, dimensions)
+        product_dataframe = work_with_excel_data(filepath, dimensions,axis_X,axis_Y)
     else:
         do_exit_msg(f"At this time files with format {check_format} is not acessible")
         return
 
     if (product_dataframe is None): return
 
+    product_dataframe_output: dict[str, DataFrame]
+    axis_X_output: list[str]
+    axis_Y_output: list[str]
+
+    product_dataframe_output, axis_X_output, axis_Y_output = product_dataframe
     result: str
 
     if (IS_DRAG_AND_DROPPED):
-        result = save_at_script_path(product_dataframe)
+        result = save_at_script_path(product_dataframe_output)
     else:
-        result = save_with_ask(product_dataframe)
+        result = save_with_ask(product_dataframe_output)
+
     print(f"SAVING TO {result}")
     try:
         with pd.ExcelWriter(result) as writer:
-            for key, df in product_dataframe.items():
-                df.to_excel(writer, sheet_name=key, header=False)
-    except:
-        do_exit_msg("Looks like is save error.")
-        return
+            for key, df in product_dataframe_output.items():
+                df.index = axis_Y_output
+                df.to_excel(writer, sheet_name=key, header=axis_X_output)
+    except PermissionError:
+        do_exit_msg("Looks like file to save is currently openned in another program!")
         
 
-    # print("DONE")
+    do_exit_msg("DONE")
     
 
 
